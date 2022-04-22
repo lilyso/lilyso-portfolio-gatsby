c39f3b06f66e92cbed76b22bc940b09578f8f8ee
lilys0 <lsouryaphanh@gmail.com>
2022-04-22 23:39:27 +1000

    socials update

--- src/components/socials.js
+++ src/components/socials.js
@@ -9,17 +9,25 @@ import {
 } from "@fortawesome/free-brands-svg-icons"
 
 const Socials = () => {
-  const socialIcons = [faInstagram, faFacebook, faGithub, faLinkedin]
+  const socialIcons = [
+    { icon: faGithub, link: "https://github.com/lilyso" },
+    {
+      icon: faLinkedin,
+      link: "https://www.linkedin.com/in/lily-souryaphanh-04b16a3b/",
+    },
+  ]
   return (
     <div className="mx-4">
       {socialIcons &&
         socialIcons.map(socialIcon => (
-          <FontAwesomeIcon
-            icon={socialIcon}
-            size="2x"
-            style={{ color: "white" }}
-            className="hover:scale-150 hover:transition hover:duration-500 hover:ease-in-out p-4"
-          />
+          <a href={socialIcon.link} target="_blank">
+            <FontAwesomeIcon
+              icon={socialIcon.icon}
+              size="2x"
+              style={{ color: "white" }}
+              className="hover:scale-150 hover:transition hover:duration-500 hover:ease-in-out p-4"
+            />
+          </a>
         ))}
     </div>
   )
