8c0df96d784b664a4bde1f32c989769d3e0f4816
unknown <lsouryaphanh@gmail.com>
2023-02-02 10:44:13 +1100

    dependecy update and analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,15 +47,18 @@ module.exports = {
         short_name: `Lily So`,
         start_url: `/`,
         background_color: `#ffffff`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/icons/favicon-32x32.png`, // This path is relative to the root of the site.
+        icon: `src/images/icons/favicon-32x32.png`, 
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-C7X5K331P6", // Google Analytics / GA
+        ],
+      },
+    },
   ],
 }
